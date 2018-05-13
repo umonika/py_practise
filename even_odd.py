78705db6e89b9fa9b38c53572a65a0a83e571408
umonika <39208561+umonika@users.noreply.github.com>
2018-05-13 16:12:58 +0530

    Update even_odd.py

--- even_odd.py
+++ even_odd.py
@@ -1,28 +1,14 @@
 from random import randint
-
 num_list = []
-
 for trk in  range(10):
 
     num_list.append(randint(1,10))
-
-
 print('num_list',num_list)
-
 even_odd_dict={}
-
-
-
 for a_num in num_list:
-
     if a_num % 2 == 0:
-
         even_odd_dict[a_num] = "Even "
-
     else:
-
         even_odd_dict[a_num] = " Odd "
-
-
 
 print("Even_Odd_Dict",even_odd_dict)
